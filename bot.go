f4e75347018902a056620387c52dc7f287153cb5
Marcin Puczko <nkrm@Marcins-MacBook-Pro.local>
2019-11-23 17:11:30 +0100

    add writing to file

--- bot.go
+++ bot.go
@@ -19,22 +19,29 @@ type bot struct {
 const flashcardsFileName = "flashcards.json"
 
 func defaultSendOpt(m *tba.Message) *tba.SendOptions {
+
 	return &tba.SendOptions{
+
 		ReplyTo: m,
 	}
 }
 
 func ensureDataFileExists(fileName string) {
+
 	if _, err := os.Stat(fileName); err != nil {
+
 		err = ioutil.WriteFile(fileName, []byte("{}"), 0644)
 		if err != nil {
+
 			log.Fatalf("could not create %s", fileName)
 		}
 	}
 }
 
 func GetAnswer(b *bot, chatID int64, answer chan string) {
+
 	b.api.Handle(tba.OnText, func(m *tba.Message) {
+
 		if chatID == m.Chat.ID {
 			answer <- m.Text
 		}
@@ -42,13 +49,16 @@ func GetAnswer(b *bot, chatID int64, answer chan string) {
 }
 
 func SendMessage(b *bot, chat int64, message string, sendOpt *tba.SendOptions) error {
+
 	tmpChat := tba.Chat{ID: chat, Title: "", FirstName: "", LastName: "", Type: "", Username: ""}
 	_, err := b.api.Send(&tmpChat, message, sendOpt)
 
 	return err
 }
 
+//TODO: make it work for multiple users at the same time
 func addFlashcard(b *bot, chatID int64) {
+
 	subjectChan := make(chan string)
 	SendMessage(b, chatID, "Podaj temat fiszki", &tba.SendOptions{})
 	GetAnswer(b, chatID, subjectChan)
@@ -65,48 +75,76 @@ func addFlashcard(b *bot, chatID int64) {
 	definition := <-definitionChan
 
 	if b.flashcards[chatID] == nil {
+
 		b.flashcards[chatID] = make(map[string]map[string]string)
 	}
 	if b.flashcards[chatID][subject] == nil {
+
 		b.flashcards[chatID][subject] = make(map[string]string)
 	}
 	b.flashcards[chatID][subject][term] = definition
 
-	log.Print(b.flashcards[chatID])
+	flashcardsJson, err := json.Marshal(b.flashcards)
+	if err != nil {
+		log.Print("Could not encode flashcards")
+		SendMessage(b, chatID, "Wystapil problem, sprobuj pozniej", &tba.SendOptions{})
+		return
+	}
+	err = ioutil.WriteFile(flashcardsFileName, flashcardsJson, 0644)
+	if err != nil {
+		log.Print("Could not write flashcards")
+		SendMessage(b, chatID, "Wystapil problem, sprobuj pozniej", &tba.SendOptions{})
+		return
+	}
+
+	SendMessage(b, chatID, "Dodano fiszke", &tba.SendOptions{})
 
 }
 
 func (b *bot) Run() {
+
 	b.api.Handle("/version", func(m *tba.Message) {
-		err := SendMessage(b, m.Chat.ID, "version 0.0.1", defaultSendOpt(m))
+
+		err := SendMessage(b, m.Chat.ID, "version 0.0.2", defaultSendOpt(m))
 		if err != nil {
+
 			log.Printf("error sending version to %d", m.Chat.ID)
 		}
 	})
+
 	b.api.Handle("/dodajfiszke", func(m *tba.Message) {
+
 		go addFlashcard(b, m.Chat.ID)
 	})
-
 	b.api.Start()
 }
 
 func NewBot(token string) *bot {
+
 	tb, err := tba.NewBot(tba.Settings{
+
 		Token:  token,
 		Poller: &tba.LongPoller{Timeout: 10 * time.Second},
 	})
+
 	if err != nil {
+
 		log.Fatal("Could not create bot")
 	}
 
 	flashcards := make(map[int64]map[string]map[string]string)
 	ensureDataFileExists(flashcardsFileName)
 	flashcardsData, err := ioutil.ReadFile(flashcardsFileName)
+
 	if err != nil {
+
 		log.Fatalf("Could not read %s", flashcardsFileName)
 	}
+
 	err = json.Unmarshal([]byte(flashcardsData), &flashcards)
+
 	if err != nil {
+
 		log.Fatalf("Could not decode %s", flashcardsFileName)
 	}
 
